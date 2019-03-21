4f3de4ba476a3d655f838cd44cd6f3eedfb98b87
Nikhil Barhate <nikhilbarhate99@gmail.com>
2019-03-21 16:02:01 +0530

    Update PPO.py

--- PPO.py
+++ PPO.py
@@ -13,6 +13,9 @@ class Model(nn.Module):
         # actor
         self.action_layer = nn.Sequential(
                 nn.Linear(state_dim, n_latent_var),
+                nn.Tanh(),
+                nn.Linear(n_latent_var, n_latent_var),
+                nn.Tanh(),
                 nn.Linear(n_latent_var, action_dim),
                 nn.Softmax()
                 )
@@ -20,6 +23,9 @@ class Model(nn.Module):
         # critic
         self.value_layer = nn.Sequential(
                 nn.Linear(state_dim, n_latent_var),
+                nn.Tanh(),
+                nn.Linear(n_latent_var, n_latent_var),
+                nn.Tanh(),
                 nn.Linear(n_latent_var, 1)
                 )
         
@@ -48,6 +54,9 @@ class Model(nn.Module):
         self.logprobs.append(action_distribution.log_prob(action))
         self.state_values.append(state_value)
         
+        if evaluate:
+            return action_distribution.entropy().mean()
+        
         if not evaluate:
             return action.item()
         
@@ -71,7 +80,7 @@ class PPO:
                                               lr=lr, betas=betas)
         self.policy_old = Model(state_dim, action_dim, n_latent_var).to(device)
         
-        self.SmoothL1Loss = nn.SmoothL1Loss()
+        self.MseLoss = nn.MSELoss()
         
     def update(self):   
         # Monte Carlo estimate of state rewards:
@@ -83,7 +92,7 @@ class PPO:
         
         # Normalizing the rewards:
         rewards = torch.tensor(rewards).to(device)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-10)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
         
         # convert list in tensor
         old_states = torch.tensor(self.policy_old.states).to(device).detach()
@@ -93,7 +102,7 @@ class PPO:
         # Optimize policy for K epochs:
         for _ in range(self.K_epochs):
             # Evaluating old actions and values :
-            self.policy(old_states, old_actions, evaluate=True)
+            dist_entropy = self.policy(old_states, old_actions, evaluate=True)
             
             # Finding the ratio (pi_theta / pi_theta__old):
             logprobs = self.policy.logprobs[0].to(device)
@@ -104,7 +113,7 @@ class PPO:
             advantages = rewards - state_values.detach()
             surr1 = ratios * advantages
             surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip)
-            loss = -torch.min(surr1, surr2) + self.SmoothL1Loss(state_values, rewards)
+            loss = -torch.min(surr1, surr2) + 0.5*self.MseLoss(state_values, rewards) - 0.01*dist_entropy
             
             # take gradient step
             self.optimizer.zero_grad()
@@ -131,11 +140,11 @@ def main():
     log_interval = 10
     n_latent_var = 64           # number of variables in hidden layer
     n_update = 10               # update policy every n episodes
-    lr = 0.0008
+    lr = 0.0007
     betas = (0.9, 0.999)
     gamma = 0.99                # discount factor
     K_epochs = 5                # update policy for K epochs
-    eps_clip = 0.1              # clip parameter for PPO
+    eps_clip = 0.2              # clip parameter for PPO
     random_seed = None
     #############################################
     
@@ -153,13 +162,13 @@ def main():
         for t in range(10000):
             # Running policy_old:
             action = ppo.policy_old(state)
-            state_new, reward, done, _ = env.step(action)
+            state_n, reward, done, _ = env.step(action)
             
             # Saving state and reward:
             ppo.policy_old.states.append(state)
             ppo.policy_old.rewards.append(reward)
             
-            state = state_new
+            state = state_n
             
             running_reward += reward
             if render:
@@ -192,7 +201,6 @@ def main():
             
 if __name__ == '__main__':
     main()
-    
     
     
     
