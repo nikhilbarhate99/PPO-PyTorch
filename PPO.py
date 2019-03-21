f33c504cdb96c69b25a092ab7fda382a6889cbd4
Nikhil Barhate <nikhilbarhate99@gmail.com>
2019-03-21 11:35:03 +0530

    Update PPO.py

--- PPO.py
+++ PPO.py
@@ -1,16 +1,27 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.distributions import Categorical
 import gym
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 class Model(nn.Module):
-    def __init__(self, n_latent_var):
+    def __init__(self, state_dim, action_dim, n_latent_var):
         super(Model, self).__init__()
-        self.affine = nn.Linear(8, n_latent_var)
+        self.affine = nn.Linear(state_dim, n_latent_var)
         
-        self.action_layer = nn.Linear(n_latent_var, 4)
-        self.value_layer = nn.Linear(n_latent_var, 1)
+        # actor
+        self.action_layer = nn.Sequential(
+                nn.Linear(state_dim, n_latent_var),
+                nn.Linear(n_latent_var, action_dim),
+                nn.Softmax()
+                )
+        
+        # critic
+        self.value_layer = nn.Sequential(
+                nn.Linear(state_dim, n_latent_var),
+                nn.Linear(n_latent_var, 1)
+                )
         
         # Memory:
         self.actions = []
@@ -20,14 +31,14 @@ class Model(nn.Module):
         self.rewards = []
         
     def forward(self, state, action=None, evaluate=False):
-        # if evaluate is True then we also need pass an action for evaluation
+        # if evaluate is True then we also need to pass an action for evaluation
         # else we return a new action from distribution
-        state = torch.from_numpy(state).float()
-        state = self.affine(state)
+        if not evaluate:
+            state = torch.from_numpy(state).float().to(device)
         
         state_value = self.value_layer(state)
         
-        action_probs = F.softmax(self.action_layer(state))
+        action_probs = self.action_layer(state)
         action_distribution = Categorical(action_probs)
         
         if not evaluate:
@@ -37,7 +48,8 @@ class Model(nn.Module):
         self.logprobs.append(action_distribution.log_prob(action))
         self.state_values.append(state_value)
         
-        return action.item()
+        if not evaluate:
+            return action.item()
         
     def clearMemory(self):
         del self.actions[:]
@@ -47,59 +59,58 @@ class Model(nn.Module):
         del self.rewards[:]
         
 class PPO:
-    def __init__(self, n_latent_var, lr, betas, gamma, K_epochs, eps_clip):
+    def __init__(self, state_dim, action_dim, n_latent_var, lr, betas, gamma, K_epochs, eps_clip):
         self.lr = lr
         self.betas = betas
         self.gamma = gamma
         self.eps_clip = eps_clip
         self.K_epochs = K_epochs
         
-        self.policy = Model(n_latent_var)
+        self.policy = Model(state_dim, action_dim, n_latent_var).to(device)
         self.optimizer = torch.optim.Adam(self.policy.parameters(),
                                               lr=lr, betas=betas)
-        self.policy_old = Model(n_latent_var)
+        self.policy_old = Model(state_dim, action_dim, n_latent_var).to(device)
+        
+        self.SmoothL1Loss = nn.SmoothL1Loss()
         
     def update(self):   
         # Monte Carlo estimate of state rewards:
         rewards = []
         discounted_reward = 0
-        for reward in self.policy_old.rewards[::-1]:
+        for reward in reversed(self.policy_old.rewards):
             discounted_reward = reward + (self.gamma * discounted_reward)
             rewards.insert(0, discounted_reward)
         
         # Normalizing the rewards:
-        rewards = torch.tensor(rewards)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+        rewards = torch.tensor(rewards).to(device)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-10)
+        
+        # convert list in tensor
+        old_states = torch.tensor(self.policy_old.states).to(device).detach()
+        old_actions = torch.tensor(self.policy_old.actions).to(device).detach()
+        old_logprobs = torch.tensor(self.policy_old.logprobs).to(device).detach()
         
         # Optimize policy for K epochs:
         for _ in range(self.K_epochs):
             # Evaluating old actions and values :
-            for state, action in zip(self.policy_old.states, 
-                                     self.policy_old.actions):
-                self.policy(state, action, evaluate=True)
+            self.policy(old_states, old_actions, evaluate=True)
             
             # Finding the ratio (pi_theta / pi_theta__old):
-            ratios = []
-            for logprob, logprob_old in zip(self.policy.logprobs, 
-                                            self.policy_old.logprobs):
-                ratios.append(torch.exp(logprob - logprob_old.item()))
-            
+            logprobs = self.policy.logprobs[0].to(device)
+            ratios = torch.exp(logprobs - old_logprobs.detach())
+                
             # Finding Surrogate Loss:
-            loss = 0
-            action_loss = 0
-            for ratio, value, reward in zip(ratios, self.policy.state_values,
-                                            rewards):
-                advantage = reward - value.item()
-                surr1 = ratio * advantage
-                surr2 = torch.clamp(ratio, 1-self.eps_clip, 1+self.eps_clip) * advantage
-                action_loss = -torch.min(surr1, surr2)
-                value_loss = F.smooth_l1_loss(value, reward)
-                loss += (action_loss + value_loss)
-            loss = loss.mean()
+            state_values = self.policy.state_values[0].to(device)
+            advantages = rewards - state_values.detach()
+            surr1 = ratios * advantages
+            surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip)
+            loss = -torch.min(surr1, surr2)
             
+            # take gradient step
             self.optimizer.zero_grad()
-            loss.backward()
+            loss.mean().backward()
             self.optimizer.step()
+            
             self.policy.clearMemory()
         
         self.policy_old.clearMemory()
@@ -108,22 +119,31 @@ class PPO:
         self.policy_old.load_state_dict(self.policy.state_dict())
         
 def main():
-    env = gym.make('LunarLander-v2')
+    ############## Hyperparameters ##############
+    env_name = "LunarLander-v2"
+    #env_name = "CartPole-v1"
+    # creating environment
+    env = gym.make(env_name)
+    state_dim = env.observation_space.shape[0]
+    action_dim = 4
+    
     render = False
     log_interval = 10
-    n_latent_var = 128
-    lr = 0.0003
+    n_latent_var = 64           # number of variables in hidden layer
+    n_update = 10               # update policy every n episodes
+    lr = 0.0008
     betas = (0.9, 0.999)
-    gamma = 0.99
-    K_epochs = 5 # update policy for K epochs
-    eps_clip = 0.2 # clip parameter for PPO
-    
+    gamma = 0.99                # discount factor
+    K_epochs = 5                # update policy for K epochs
+    eps_clip = 0.1              # clip parameter for PPO
     random_seed = None
-    if random_seed is not None:
+    #############################################
+    
+    if random_seed:
         torch.manual_seed(random_seed)
         env.seed(random_seed)
         
-    ppo = PPO(n_latent_var, lr, betas, gamma, K_epochs, eps_clip)
+    ppo = PPO(state_dim, action_dim, n_latent_var, lr, betas, gamma, K_epochs, eps_clip)
     print(lr,betas)
     
     running_reward = 0
@@ -144,9 +164,14 @@ def main():
                 env.render()
             if done:
                 break
-        ppo.update()
+        
         avg_length += t
         
+        # update after n episodes
+        if i_episode % n_update:
+            ppo.update()
+        
+        # log
         if running_reward > (log_interval*200):
             print("########## Solved! ##########")
             torch.save(ppo.policy.state_dict(), 
@@ -165,6 +190,11 @@ def main():
             
 if __name__ == '__main__':
     main()
+    
+    
+    
+    
+    
     
     
     
